60d77d66cb37f307f8aef62f2d90eb9fe92a22df
mchipperfield <189554036+mchipperfield@users.noreply.github.com>
2025-05-24 20:59:44 +0100

    refactor to reduce load on main.go

---
+++ handlers.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"log/slog"
+	"math/rand/v2"
+	"strings"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func getQuote(serverId string, quotes []string) func(s *discordgo.Session, m *discordgo.MessageCreate) {
+	return func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Content == "!quote" {
+			if m.GuildID != serverId {
+				return
+			}
+
+			if m.Author.ID == s.State.User.ID {
+				return
+			}
+
+			n := rand.IntN(len(quotes) - 1)
+			_, err := s.ChannelMessageSend(m.ChannelID, quotes[n])
+			if err != nil {
+				slog.Info("failed to send message", "error", err, "channel", m.ChannelID)
+			}
+		}
+	}
+}
+
+func hungry(serverId string) func(s *discordgo.Session, m *discordgo.MessageCreate) {
+	return func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.GuildID != serverId {
+			return
+		}
+
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if strings.Contains(strings.ToLower(m.Content), "hungry") {
+			user := m.Author.ID
+			_, err := s.ChannelMessageSend(m.ChannelID, "<@"+user+"> Hungry? You know where the fridge is!")
+			if err != nil {
+				slog.Info("failed to send message", "error", err, "channel", m.ChannelID)
+			}
+		}
+	}
+}
+
+func wakeUp(serverId string, opus [][]byte) func(s *discordgo.Session, m *discordgo.MessageCreate) {
+	return func(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+		if m.GuildID != serverId {
+			return
+		}
+
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if strings.Contains(m.Content, "tired") {
+
+			guild, err := s.State.Guild(m.GuildID)
+			if err != nil {
+				slog.Info("failed to get guild", "id", m.GuildID, "error", err)
+			}
+			for _, vs := range guild.VoiceStates {
+				if vs.UserID == m.Author.ID {
+					vc, err := s.ChannelVoiceJoin(m.GuildID, vs.ChannelID, false, false)
+					if err != nil {
+						slog.Info("failed to join voice channel", "error", err, "id", vs.ChannelID)
+					}
+
+					if err := vc.Speaking(true); err != nil {
+						slog.Info("failed to start speaking", "error", err)
+					}
+					time.Sleep(250 * time.Millisecond)
+					for _, buf := range opus {
+						vc.OpusSend <- buf
+					}
+
+					if err := vc.Speaking(false); err != nil {
+						slog.Info("failed to start speaking", "error", err)
+					}
+					vc.Disconnect()
+				}
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -6,12 +6,9 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
-	"math/rand/v2"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
-	"time"
 
 	"github.com/peterbourgon/ff"
 
@@ -30,18 +27,17 @@ var quotes = []string{
 	"i'm always prepared!",
 	"Up the Spurs!",
 	"SEND OUT!!!",
+	"You are ENOUGH!",
 }
 
 // Server2985 is the guild id for the SD server.
 const Server2985 = "1339671620880699433"
 
-var buffer = make([][]byte, 0)
-
 func main() {
 	fs := flag.NewFlagSet("", flag.ContinueOnError)
 	var (
-		token  = fs.String("bot_token", "", "bot authentication token")
-		server = fs.String("server", Server2985, "server to listen on")
+		token    = fs.String("bot_token", "", "bot authentication token")
+		serverId = fs.String("server_id", Server2985, "server to listen on")
 	)
 	if err := ff.Parse(fs,
 		os.Args[1:],
@@ -51,7 +47,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := loadSound("wake_up.dca"); err != nil {
+	opus, err := loadSound("wake_up.dca")
+	if err != nil {
 		slog.Info("failed to load sound", "error", err)
 		os.Exit(1)
 	}
@@ -62,61 +59,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.GuildID != *server {
-			return
-		}
-
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		if strings.Contains(strings.ToLower(m.Content), "hungry") {
-			user := m.Author.ID
-			_, err := s.ChannelMessageSend(m.ChannelID, "<@"+user+"> Hungry? You know where the fridge is!")
-			if err != nil {
-				slog.Info("failed to send message", "error", err, "channel", m.ChannelID)
-			}
-		}
-
-		if m.Content == "!quote" {
-			n := rand.IntN(len(quotes) - 1)
-			_, err := s.ChannelMessageSend(m.ChannelID, quotes[n])
-			if err != nil {
-				slog.Info("failed to send message", "error", err, "channel", m.ChannelID)
-			}
-		}
-
-		if strings.Contains(m.Content, "tired") {
-
-			guild, err := s.State.Guild(m.GuildID)
-			if err != nil {
-				slog.Info("failed to get guild", "id", m.GuildID, "error", err)
-			}
-			for _, vs := range guild.VoiceStates {
-				if vs.UserID == m.Author.ID {
-					vc, err := s.ChannelVoiceJoin(m.GuildID, vs.ChannelID, false, false)
-					if err != nil {
-						slog.Info("failed to join voice channel", "error", err, "id", vs.ChannelID)
-					}
-
-					if err := vc.Speaking(true); err != nil {
-						slog.Info("failed to start speaking", "error", err)
-					}
-					time.Sleep(250 * time.Millisecond)
-					for _, buf := range buffer {
-						vc.OpusSend <- buf
-					}
-
-					if err := vc.Speaking(false); err != nil {
-						slog.Info("failed to start speaking", "error", err)
-					}
-					vc.Disconnect()
-				}
-			}
-
-		}
-	})
+	session.AddHandler(hungry(*serverId))
+	session.AddHandler(getQuote(*serverId, quotes))
+	session.AddHandler(wakeUp(*serverId, opus))
 
 	if err := session.Open(); err != nil {
 		slog.Info("error opening websocket", "error", err)
@@ -140,13 +85,13 @@ func main() {
 }
 
 // loadSound copy/pasta from the !airhorn example at bwmarrin/godiscord
-func loadSound(filename string) error {
+func loadSound(filename string) ([][]byte, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("Error opening dca file :", err)
-		return err
+		return nil, err
 	}
-
+	var buffer = make([][]byte, 0)
 	var opuslen int16
 
 	for {
@@ -157,14 +102,14 @@ func loadSound(filename string) error {
 		if err == io.EOF || err == io.ErrUnexpectedEOF {
 			err := file.Close()
 			if err != nil {
-				return err
+				return nil, err
 			}
-			return nil
+			return buffer, nil
 		}
 
 		if err != nil {
 			fmt.Println("Error reading from dca file :", err)
-			return err
+			return nil, err
 		}
 
 		// Read encoded pcm from dca file.
@@ -174,10 +119,11 @@ func loadSound(filename string) error {
 		// Should not be any end of file errors
 		if err != nil {
 			fmt.Println("Error reading from dca file :", err)
-			return err
+			return nil, err
 		}
 
 		// Append encoded pcm data to the buffer.
 		buffer = append(buffer, InBuf)
 	}
+
 }
